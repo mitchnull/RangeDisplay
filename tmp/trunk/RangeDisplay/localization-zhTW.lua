7ed43155c0918465c328183cf2990649640cc208
mitchell <mitchell@be86e551-2199-4a07-843e-6d7f9421a4f6>
2007-07-04 08:08:23 +0000

    RangeDisplay: preparing files for localizers

---
+++ tmp/trunk/RangeDisplay/localization-zhTW.lua
@@ -0,0 +1,19 @@
+local L = AceLibrary("AceLocale-2.2"):new("RangeDisplay")
+L:RegisterTranslations("zhTW", function() return {
+		
+-- ["Enabled"] = true,
+-- ["Enable/Disable the mod"] = true,
+-- ["Locked"] = true,
+-- ["Lock/Unlock display frame"] = true,
+-- ["Show out of range"] = true,
+-- ["Display max checked range if the unit is out of range, or hide the display"] = true,
+-- ["Check visibility"] = true,
+-- ["If set, the max range will be 'visibility range'"] = true,
+-- ["Font size"] = true,
+-- ["Set the font size"] = true,
+-- ["Reset"] = true,
+-- ["Restore default settings"] = true,
+-- ["RangeDisplay"] = true,
+
+} end)
+
