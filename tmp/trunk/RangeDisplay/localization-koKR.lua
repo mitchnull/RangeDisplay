1d01f9cf37431bfd894b0039167a7e996a6f1dbe
sayclub <sayclub@be86e551-2199-4a07-843e-6d7f9421a4f6>
2007-07-04 11:09:11 +0000

    RangeDisplay: Added translation koKR

--- tmp/trunk/RangeDisplay/localization-koKR.lua
+++ tmp/trunk/RangeDisplay/localization-koKR.lua
@@ -1,19 +1,19 @@
-local L = AceLibrary("AceLocale-2.2"):new("RangeDisplay")
+﻿local L = AceLibrary("AceLocale-2.2"):new("RangeDisplay")
 L:RegisterTranslations("koKR", function() return {
 		
--- ["Enabled"] = true,
--- ["Enable/Disable the mod"] = true,
--- ["Locked"] = true,
--- ["Lock/Unlock display frame"] = true,
--- ["Show out of range"] = true,
--- ["Display max checked range if the unit is out of range, or hide the display"] = true,
--- ["Check visibility"] = true,
--- ["If set, the max range will be 'visibility range'"] = true,
--- ["Font size"] = true,
--- ["Set the font size"] = true,
--- ["Reset"] = true,
--- ["Restore default settings"] = true,
--- ["RangeDisplay"] = true,
+ ["Enabled"] = "사용",
+ ["Enable/Disable the mod"] = "모드를 사용하거나 사용하지 않습니다.",
+ ["Locked"] = "잠금",
+ ["Lock/Unlock display frame"] = "화면 프레임을 잠그거나 이동시킵니다.",
+ ["Show out of range"] = "사정 거리 벗어남 표시",
+ ["Display max checked range if the unit is out of range, or hide the display"] = "대상이 사정 거리를 벗어날 경우 최대 체크 거리를 표시합니다. 그렇지 않으면 화면에서 숨김니다.",
+ ["Check visibility"] = "눈에 보이는 범위 체크",
+ ["If set, the max range will be 'visibility range'"] = "만약 설정시, 최대 범위는 '눈에 보이는 범위'입니다.",
+ ["Font size"] = "글꼴 크기",
+ ["Set the font size"] = "글꼴 크기를 설정합니다.",
+ ["Reset"] = "초기화",
+ ["Restore default settings"] = "기본 설정값으로 초기화 합니다.",
+ ["RangeDisplay"] = "RangeDisplay",
 
 } end)
 
