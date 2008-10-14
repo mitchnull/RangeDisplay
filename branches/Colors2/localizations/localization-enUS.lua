6649bb0d364d932920c0facaa858a3710f1b183d
mitch0 <mitch0@be86e551-2199-4a07-843e-6d7f9421a4f6>
2008-10-14 08:58:15 +0000

    restarting branch for easier merge later...

---
+++ branches/Colors2/localizations/localization-enUS.lua
@@ -0,0 +1,37 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("RangeDisplay", "enUS", true)
+if not L then return end
+		
+L["Locked"] = true
+L["Lock/Unlock display frame"] = true
+L["Out of range display"] = true
+L["Show/Hide display if the target is out of range"] = true
+L["Check visibility"] = true
+L["If set, the max range to check will be 'visibility range'"] = true
+L["Max range only"] = true
+L["Show the maximum range only"] = true
+L["Font"] = true
+L["Default"] = true
+L["Font size"] = true
+L["Font outline"] = true
+L["None"] = true
+L["Normal"] = true
+L["Thick"] = true
+L["Color"] = true
+L["%s loaded. Type /rangedisplay for help"] = true
+L["Enemy only"] = true
+L["Show range for enemy targets only"] = true
+L["Strata"] = true
+L["Frame strata"] = true
+L["High"] = true
+L["Medium"] = true
+L["Low"] = true
+L["Configure"] = true
+L["Bring up GUI configure dialog"] = true
+L["Out of range color"] = true
+L["Suffix"] = true
+L["A free-form suffix to append to the range display when you are in range"] = true
+L["Out of range suffix"] = true
+L["A free-form suffix to append to the range display when you are out of range"] = true
+
+L["Range Display"] = true
+L["Estimated range display"] = true
