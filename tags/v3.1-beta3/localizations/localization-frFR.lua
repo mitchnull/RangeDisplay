03ed98c3f99e4eecb7d4b9ba5d6ddd2af5ea846a
mitch0 <mitch0@be86e551-2199-4a07-843e-6d7f9421a4f6>
2008-10-19 16:31:15 +0000

    tagging 3.1-beta3

---
+++ tags/v3.1-beta3/localizations/localization-frFR.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("RangeDisplay", "frFR", false)
+if not L then return end
+
+-- frFR by |Pixel|
+
+L["Locked"] = "Vérouillé"
+L["Lock/Unlock display frame"] = "(Dé)Vérouille la fenêtre"
+L["Out of range display"] = "Affichage de la portée"
+L["Show/Hide display if the target is out of range"] = "Affiche/cache l'affichage si la cible est hors de portée"
+L["Check visibility"] = "Visibilité"
+L["If set, the max range to check will be 'visibility range'"] = "Si actif, la portée à vérifier sera la visibilité"
+-- L["Max range only"] = true
+-- L["Show the maximum range only"] = true
+L["Font"] = "Police"
+L["Default"] = "Défaut"
+L["Font size"] = "Taille de police"
+L["Font outline"] = "Epaisseur de police"
+L["None"] = "Aucun"
+L["Normal"] = "Normal"
+L["Thick"] = "Epais"
+L["Color"] = "Couleur"
+L["%s loaded. Type /rangedisplay for help"] = "%s chargé. Tapez /rangedisplay pour obtenir de l'aide"
+L["Enemy only"] = "Ennemis seulement"
+L["Show range for enemy targets only"] = "Affiche la portée pour les ennemis seulement"
+L["Strata"] = "Profondeur"
+L["Frame strata"] = "Profondeur de la fenêtre"
+L["High"] = "Au dessus"
+L["Medium"] = "Normal"
+L["Low"] = "En dessous"
+-- L["Configure"] = true
+-- L["Bring up GUI configure dialog"] = true
